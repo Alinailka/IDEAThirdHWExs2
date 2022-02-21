7f1e7d75935b3383378446f567c483673d0852b0
Alinailka <alina-il@mail.ru>
2022-02-21 21:16:56 +0700

    Second commit:

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,5 @@
 public class BmiService {
-    public double calculate(
-            double height, double weight
-    ) {
+    public double calculate(double height, double weight) {
         double heightSquare = height * height;
         double ibm = weight / heightSquare;
         return ibm;
